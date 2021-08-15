b1585e70c6406b8084ad05bea64b2fa360626bcd
monaSalih <monaalobisat@gmail.com>
2021-08-15 15:53:50 +0300

    startCodeing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends React.Component {
+  cityLocation=()=>{
+    let locUrl='https://eu1.locationiq.com/v1/search.php?key=pk.78f49f3aca0af1e3c2fb3a2a0b800fe0&q=amman&format=json'
+
+    let showCity=axios.get(locUrl)
+    console.log(showCity);
+    
+  }
+  render() {
+    return (
+      <div>
+        <>
+        <h2>City Explorer</h2>
+        <button onClick={this.cityLocation}> Get city location</button>
+        </>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
